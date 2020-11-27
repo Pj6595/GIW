f5956ee5570a49342e72cafd56598404864073d6
Pj <pjurad01@ucm.es>
2020-11-27 12:30:07 +0100

    Creada practica8

---
+++ Practica8/gr06_mongoengine.py
@@ -0,0 +1,10 @@
+# Jaime Antolín, Álvar Domingo, Pablo Jurado y Leire Osés declaramos que esta solución es fruto exclusivamente
+# de nuestro trabajo personal. No hemos sido ayudados por ninguna otra persona ni hemos
+# obtenido la solución de fuentes externas, y tampoco hemos compartido nuestra solución
+# con nadie. Declaramos además que no hemos realizado de manera deshonesta ninguna otra
+# actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los demás.
+
+from mongoengine import *
+from mongoengine.connection import connect
+
+connect('giw_mongoengine')
